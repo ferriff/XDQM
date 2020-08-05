03d0c8663b9ec69b40ff827a8f1662140c73271a
Federico Ferri <federico.ferri@cern.ch>
2020-08-05 14:12:50 +0200

    properly accumulate, add correlations, add several improvements
       * add correlations to the plots
       * dump and (proper) restore from pickle objects
       * use internal counter for the number of analyzed samples
          * also use it for properly adding `accumulator' objects
       * easier handle of information extension for peaks and baselines
       * improve fft averages
          * better account of the average set size
          * better check of FFT consistency across add operations
       * improve documentation

--- accumulator.py
+++ accumulator.py
@@ -10,26 +10,35 @@ import pickle
 class accumulator:
     """Accumulates plotting information"""
 
+
     def __init__(self):
         self.det = {}
 
+
     def add(self, det, obs, values):
-        """Add to the detector `det' the observable `obs' with values `values'. If `obs' already exists, `values' are extended assuming no time overlap."""
+        """Add to the detector `det' the observable `obs' with values `values'. If `obs' already exists, `values' are extended assuming no time overlap. The number of analyzed samples is NOT updated."""
         if det not in self.det.keys():
             self.det[det] = detinfo()
         self.det[det].add(obs, values)
 
+
     def add_acc(self, acc):
-        """Extend the accumulated information with that contained in `acc'."""
+        """Extend the accumulated information with that contained in `acc'. The latter is assumed to have analyzed samples starting from 0. The number of analyzed samples is used to properly extend the relevant observables and the final one automatically updated."""
         if self.det.keys() != acc.det.keys():
             print("Error, cannot add two different detinfo objects:\n-->", self.det.keys(), "-->", acc.det.keys())
             import sys
             sys.exit(1)
-        for key, val in self.det.values():
-            self.det[det].add_det(acc[det])
+        for key, val in self.det.items():
+            val.add_det(acc.det[key])
+
+
+    def add_analyzed_samples(self, det, nsamples):
+        self.det[det].n_analyzed_samples += nsamples
+
 
     def plot(self):
         """Plot the stored information."""
+
         # plot channels
         for det in self.det.keys():
             suff = '_det%03d' % (det - 1)
@@ -43,29 +52,45 @@ class accumulator:
             plt.plot_baseline(d.baseline, d.baseline_min, suff, det)
             # average fft
             plt.plot_fft_data(d.fft_f, d.fft, suff, det)
-            # rate for signals above threshold
-            rate = ana.compute_rate(d.peak, d.peak_max, 100 * 1e3)
-            plt.plot_rate(rate, 100, suff, det)
+            ### rate for signals above threshold
+            ##rate = ana.compute_rate(d.peak, d.peak_max, 100 * 1e3)
+            ##plt.plot_rate(rate, 100, suff, det)
+
+        # plot correlations
+        corr = [list(map(int, x.split(':'))) for x in cfg.cfg['setup']['correlations'].split()]
+        for pair in corr:
+            da = self.det[pair[0]]
+            db = self.det[pair[1]]
+            ##ana.find_correlation_window(pair[0], da.peak, pair[1], db.peak)
+            ca, cam, cb, cbm = ana.compute_correlations(da.peak, da.peak_max, db.peak, db.peak_max)
+            suff = '_det%03d' % (pair[0] - 1)
+            plt.plot_correlations(ca, cam, pair[0], cb, cbm, pair[1], suff)
+        
+
 
     def clear(self):
         """Clear detector information."""
         for key, val in self.det.items():
             val.clear()
 
+
     def dump(self, filename):
         """Dump the stored information to a file."""
         f = open(filename, 'wb')
         pickle.dump(self, f)
 
-    def load(self, filename):
-        """Load the information from a file."""
-        f = open(filename, 'rb')
-        self = pickle.load(f)
 
     def print(self):
-        for key, val in self.det.values():
+        for key, val in self.det.items():
             print('Detector', key, '---')
             val.print()
+
+
+    @classmethod
+    def load(cls, filename):
+        """Load the information from a file."""
+        f = open(filename, 'rb')
+        return pickle.load(f)
 
 
 
@@ -81,13 +106,16 @@ class detinfo:
         self.fft_f = []
         self.fft_n = 0.
         self.rate = []
+        self.n_analyzed_samples = 0.
 
 
     def add_det(self, det):
         """Add to self the information from detinfo `det'."""
-        self.add('peak', (det.peak, det.peak_max))
-        self.add('baseline', (det.baseline, det.baseline_min))
-        self.add('fft', (det.fft, det.fft_f, det.fft_n))
+        print('-->', self.peak[:10], self.peak[-10:], det.peak[:10], det.peak[-10:])
+        self.add('peak', (list(np.add(det.peak, self.n_analyzed_samples)), det.peak_max))
+        self.add('baseline', (list(np.add(det.baseline, self.n_analyzed_samples)), det.baseline_min))
+        self.add('fft', (det.fft_f, det.fft), det.fft_n)
+        self.n_analyzed_samples += det.n_analyzed_samples
 
 
     def add(self, obs, values, n = 1):
@@ -96,39 +124,45 @@ class detinfo:
             p, p_m = values
             if len(p) == 0:
                 return
-            if len(self.peak) == 0 or self.peak[0] < p[0]:
-                i  = bisect.bisect_left(self.peak, p[0])
-                self.peak = self.peak[:i] + p
-                self.peak_max = self.peak_max[:i] + p_m
-            else:
-                print(self.peak[0], p[0])
-                print(self.peak)
-                print(p)
-                i  = bisect.bisect_left(p, self.peak[0])
-                self.peak = p[:i] + self.peak
-                self.peak_max = p_m[:i] + self.peak_max
+            self.peak = self.peak + p
+            self.peak_max = self.peak_max + p_m
+            #if len(self.peak) == 0 or self.peak[0] < p[0]:
+            #    i  = bisect.bisect_left(self.peak, p[0])
+            #    self.peak = self.peak[:i] + p
+            #    self.peak_max = self.peak_max[:i] + p_m
+            #else:
+            #    print(self.peak[0], p[0])
+            #    print(self.peak)
+            #    i  = bisect.bisect_left(p, self.peak[0])
+            #    self.peak = p[:i] + self.peak
+            #    self.peak_max = p_m[:i] + self.peak_max
         elif obs == 'baseline':
             b, b_m = values
             if len(b) == 0:
                 return
-            if len(self.baseline) == 0 or self.baseline[0] < b[0]:
-                i = bisect.bisect_left(self.baseline, b[0])
-                self.baseline = self.baseline[:i] + b
-                self.baseline_min = self.baseline_min[:i] + b_m
-            else:
-                i = bisect.bisect_left(b, self.baseline[0])
-                self.baseline = b[:i] + self.baseline
-                self.baseline_min = b_m[:i] + self.baseline_min
+            self.baseline = self.baseline + b
+            self.baseline_min = self.baseline_min + b_m
+            #if len(self.baseline) == 0 or self.baseline[0] < b[0]:
+            #    i = bisect.bisect_left(self.baseline, b[0])
+            #    self.baseline = self.baseline[:i] + b
+            #    self.baseline_min = self.baseline_min[:i] + b_m
+            #else:
+            #    i = bisect.bisect_left(b, self.baseline[0])
+            #    self.baseline = b[:i] + self.baseline
+            #    self.baseline_min = b_m[:i] + self.baseline_min
         elif obs == 'fft':
             f, pxx = values
+            if len(f) == 0:
+                return
             if len(self.fft) == 0:
                 self.fft = pxx
                 self.fft_f = f
                 self.fft_n = n
             else:
-                np.add(np.multiply(self.fft, self.fft_n), np.multiply(pxx, n)) / (self.fft_n + n)
+                self.fft = list(np.add(np.multiply(self.fft, self.fft_n), np.multiply(pxx, n)) / (self.fft_n + n))
             if len(f) != len(self.fft_f) or f[-1] != self.fft_f[-1]:
-                print('Error, FFT frequencies across chunks are not identical')
+                print('Error, FFT frequencies across chunks seems not identical:')
+                print('lengths:', len(f), len(self.fft_f), '  --  freqs:', f[-1], pxx[-1], self.fft_f[-1], f[0], pxx[0], self.fft_f[0])
             self.fft_n += n
         else:
             print('detinfo.add: object %s not supported', obs)
