f237867efdb08654da2cbdc5ad4e79cbc57df6d1
Federico Ferri <federico.ferri@cern.ch>
2020-10-16 17:12:52 +0200

    remove rate and add delta_t for coincidences

--- accumulator.py
+++ accumulator.py
@@ -6,6 +6,7 @@ import plot      as plt
 import analyze   as ana
 
 import pickle
+import sys
 
 class accumulator:
     """Accumulates plotting information"""
@@ -26,7 +27,6 @@ class accumulator:
         """Extend the accumulated information with that contained in `acc'. The latter is assumed to have analyzed samples starting from 0. The number of analyzed samples is used to properly extend the relevant observables and the final one automatically updated."""
         if self.det.keys() != acc.det.keys():
             print("Error, cannot add two different detinfo objects:\n-->", self.det.keys(), "-->", acc.det.keys())
-            import sys
             sys.exit(1)
         for key, val in self.det.items():
             val.add_det(acc.det[key])
@@ -34,6 +34,12 @@ class accumulator:
 
     def add_analyzed_samples(self, det, nsamples):
         self.det[det].n_analyzed_samples += nsamples
+
+
+    def set_sampling_freq(self, det, freq):
+        if det not in self.det.keys():
+            self.det[det] = detinfo()
+        self.det[det].sampling_freq = freq
 
 
     def plot(self):
@@ -44,6 +50,8 @@ class accumulator:
             suff = '_det%03d' % (det - 1)
 
             d = self.det[det]
+
+            cfg.params.sampling_freq = d.sampling_freq
 
             plt.plot_amplitude(d.peak_max, suff, det)
             # peaks vs time
@@ -61,11 +69,17 @@ class accumulator:
         for pair in corr:
             da = self.det[pair[0]]
             db = self.det[pair[1]]
-            ##ana.find_correlation_window(pair[0], da.peak, pair[1], db.peak)
             ca, cam, cb, cbm = ana.compute_correlations(da.peak, da.peak_max, db.peak, db.peak_max)
             suff = '_det%03d' % (pair[0] - 1)
             plt.plot_correlations(ca, cam, pair[0], cb, cbm, pair[1], suff)
+            # plot correlation window
+            dt = ana.find_correlation_window(da.peak, db.peak)
+            print('==> ', pair[0], 'vs', pair[1], dt[:50])
+            plt.plot_correlation_deltat(pair[0], pair[1], dt, suff)
         
+        # set plots done for each detector
+        for det in self.det.keys():
+            plt.plot_done(det)
 
 
     def clear(self):
@@ -105,13 +119,16 @@ class detinfo:
         self.fft = []
         self.fft_f = []
         self.fft_n = 0.
-        self.rate = []
         self.n_analyzed_samples = 0.
+        self.sampling_freq = 0.
 
 
     def add_det(self, det):
         """Add to self the information from detinfo `det'."""
         print('-->', self.peak[:10], self.peak[-10:], det.peak[:10], det.peak[-10:])
+        if self.sampling_freq != det.sampling_freq:
+            print('Error when adding detinfos, sampling frequencies are different:', self.sampling_freq, det.sampling_freq)
+            sys.exit(2)
         self.add('peak', (list(np.add(det.peak, self.n_analyzed_samples)), det.peak_max))
         self.add('baseline', (list(np.add(det.baseline, self.n_analyzed_samples)), det.baseline_min))
         self.add('fft', (det.fft_f, det.fft), det.fft_n)
@@ -159,6 +176,9 @@ class detinfo:
                 self.fft_f = f
                 self.fft_n = n
             else:
+                if (len(self.fft) != len(pxx)):
+                    print('Warning: chunk of data does not have enough points for full granularity FFT, skipping.')
+                    return
                 self.fft = list(np.add(np.multiply(self.fft, self.fft_n), np.multiply(pxx, n)) / (self.fft_n + n))
             if len(f) != len(self.fft_f) or f[-1] != self.fft_f[-1]:
                 print('Error, FFT frequencies across chunks seems not identical:')
@@ -166,7 +186,6 @@ class detinfo:
             self.fft_n += n
         else:
             print('detinfo.add: object %s not supported', obs)
-
 
     def clear(self):
         self.__init__()
@@ -176,5 +195,4 @@ class detinfo:
         print("detinfo:")
         print("     peaks:", len(self.peak))
         print("  baseline:", len(self.baseline))
-        print("      rate:", len(self.rate))
         print("       fft:", self.fft_n)
